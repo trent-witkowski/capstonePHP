1169c5b2876a8af275c975690b16f3e2309be02a
trent-witkowski <165222713+trent-witkowski@users.noreply.github.com>
2025-05-05 17:28:44 -0500

    Updated User Account Page
    Updated the page, and added a new form for editing the page

--- userAccount.php
+++ userAccount.php
@@ -53,7 +53,7 @@
     $errorMsg = 'Error fetching User information';
     $thisPage = sanitizeString(INPUT_SERVER, 'PHP_SELF');
 
-    if (sanitizeString(INPUT_GET, 'pageType') == 'edit') {
+    if (sanitizeString(INPUT_GET, 'pageType') == 'view') {
 
 
         ?>
@@ -62,34 +62,66 @@
                  - There should also be a button next to the label that allows an additional thing
                    of that field to be added to the account.
                  - This should open a small text box/form the user fills in and submits
+                 - Maybe change Address to area so actual adresses are not out there
+                 - Add image to input if possible
 -->
-        <div id="userInfoMain" class="userInfo">
-            <div id="educationDiv" class="fieldDiv">
-                <h2>Education</h2>
-                <button><img src="garbage/pencil.png" alt="Edit"></button>
-                <button><img src="garbage/plus.png" alt="Add"></button>
+        <form>
+            <div id="userInfoMain">
+                <h2>Account Information</h2>
+                <input type="submit" name="pageType" value="edit" >
+                <div class="userInfo">
+                    <p>Robo Cop</p>
+                    <p><span>Age: </span>31</p>
+                    <p>fakeemaillol</p>
+                    <p><span>Phone:</span> 111-111-1111</p>
+                    <p><span>Address:</span> 123 State St</p>
+                    <p><span>Account Created On:</span> 5/5/2025</p>
+                </div>
             </div>
-            <h3>Institution</h3>
-            <p><span>Chippewa Valley Technical College, University of Wisconsin Eau Claire</span></p>
-        </div>
+        </form>
         <!--        HTML END  -->
 
         <?php
     }
-    else { // New
+    else if (sanitizeString(INPUT_GET, 'pageType') == 'edit') { // New
         ?>
         <!--        HTML START-->
 
         <form action=<?=$thisPage?> method="post">
+            <div class="userInfoEdit">
 
-            <h2>Welcome, place username here !</h2>
-            <label for="signUpUsername" id="bookArea">Username: </label>
-            <input type="text" name="signUpUsername" id="signUpUsername">
-            <br><br>
-<!--        TODO Place a bunch of forms here  -->
-            <br><br>
-            <input type="submit" value="Submit" name="signUp">
+                <h2>Edit Account Information</h2>
+                <label for="editFirstName" id="editFirstNameLbl">First Name</label>
+                <input type="text" name="firstName" id="editFirstName">
+                <br>
+                <label for="editLastName" id="editLastNameLbl">Last Name</label>
+                <input type="text" name="lastName" id="editLastName">
+                <br>
+                <label for="editAge" id="editAgeLbl">Age</label>
+                <input type="text" name="age" id="editAge">
+                <br>
+                <label for="editEmail" id="editEmailLbl">Email</label>
+                <input type="text" name="email" id="editEmail">
+                <br>
+                <label for="editPhoneNumber" id="editPhoneNumberLbl">Phone Number</label>
+                <input type="text" name="phoneNumber" id="editPhoneNumber">
+                <br>
+                <label for="editStreet" id="editStreetLbl">Street</label>
+                <input type="text" name="street" id="editStreet">
+                <br>
+                <label for="editCountry" id="editCountryLbl">Country</label>
+                <input type="text" name="country" id="editCountry">
+                <br>
+                <label for="editState" id="editStateLbl">State</label>
+                <input type="text" name="state" id="editState">
+                <br>
+                <label for="editZip" id="editZipLbl">Zip Code</label>
+                <input type="text" name="zip" id="editZip">
+                <br><br>
 
+                <input type="submit" value="Submit" name="signUp">
+                <button>Cancel</button>
+            </div>
         </form>
         <!--        HTML END  -->
 
