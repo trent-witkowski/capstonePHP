f4bf4c6a5057d086a89ca32ea40b0c30d056f7dd
trent-witkowski <165222713+trent-witkowski@users.noreply.github.com>
2025-05-06 18:55:07 -0500

    Added help page
    Added help page

---
+++ help.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Resumate</title>
+	<link rel="stylesheet" href="css/home.css">
+	<link rel="stylesheet" href="css/header.css">
+</head>
+<body>
+<div class="banner">
+	<h1>Resumate</h1>
+	<span><a href="login.php?pageType=login">Login/Sign up</a></span>
+</div>
+<div class="navDiv">
+	<table class="navTable">
+		<tbody>
+		<!--            This is the nav, painted by using a table-->
+		<tr>
+			<td></td>
+			<td class="navCell">
+				<span><a href="index.php">Home</a></span>
+			</td>
+			<td class="navCell">
+				<span><a href="help.php">Help</a></span>
+			</td>
+			<td class="navCell">
+				<span><a href="resume.php?pageType=view">Resume</a></span>
+			</td>
+			<td colspan="2" class="navCell">
+				<span><a href="userAccount.php?pageType=view">Account</a></span>
+			</td>
+		</tr>
+		</tbody>
+	</table>
+</div>
+
+<div class="main">
+	<p>No help here. Figure it out yourself</p>
+</div>
+<div class="footerDiv">
+
+</div>
+</body>
+</html>
