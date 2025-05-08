8a03a15095b676fcb61213894aee3e7ac17b60e6
actown01 <actown01@hotmail.com>
2025-05-07 19:33:01 -0500

    classes
    added user, userType

---
+++ classes/userType.php
@@ -0,0 +1,10 @@
+<?php
+
+class userType {
+  
+  // database column name is userType, pull from there
+  // verify the userType 0 (regular user), 1 (employer)
+  // display info based on userType
+
+}
+
