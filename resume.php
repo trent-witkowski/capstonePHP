6ab68922ff4ebdef8d2bc229300a32520a0eb4f0
trent-witkowski <165222713+trent-witkowski@users.noreply.github.com>
2025-05-17 21:10:04 -0500

    Update resume.php
    Got CSS back up and running
    Pull in data

--- resume.php
+++ resume.php
@@ -34,10 +34,10 @@ if (!$userID) {
     exit();
 }
 
-$selectedResumeId = sanitizeString(INPUT_GET, 'resumeId');
-
+$query = "SELECT resumeId FROM Resume WHERE userId = '$userID'";
+$resume = callQuery($pdo, $query, "Error retrieving user's resume information. ")->fetch();
+$resumeId = $resume['resumeId'];
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
-    $selectedResumeId = $_POST['resumeId'];
 
     // Handle Education
     $eduIds = $_POST['educationId'] ?? [];
@@ -48,7 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
     $ends = $_POST['endDate'] ?? [];
 
     $existingEduStmt = $pdo->prepare("SELECT educationId FROM Education WHERE resumeId = ?");
-    $existingEduStmt->execute([$selectedResumeId]);
+    $existingEduStmt->execute([$resumeId]);
     $existingEduIds = $existingEduStmt->fetchAll(PDO::FETCH_COLUMN);
 
     $postedEduIds = array_filter($eduIds);
@@ -66,7 +66,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
             $update->execute([$institutions[$i], $degrees[$i], $fields[$i], $starts[$i], $ends[$i], $id]);
         } else {
             $insert = $pdo->prepare("INSERT INTO Education (resumeId, institutionName, degree, fieldOfStudy, startDate, endDate) VALUES (?, ?, ?, ?, ?, ?)");
-            $insert->execute([$selectedResumeId, $institutions[$i], $degrees[$i], $fields[$i], $starts[$i], $ends[$i]]);
+            $insert->execute([$resumeId, $institutions[$i], $degrees[$i], $fields[$i], $starts[$i], $ends[$i]]);
         }
     }
 
@@ -79,7 +79,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
     $wends = $_POST['workEndDate'] ?? [];
 
     $existingWorkStmt = $pdo->prepare("SELECT workId FROM Work WHERE resumeId = ?");
-    $existingWorkStmt->execute([$selectedResumeId]);
+    $existingWorkStmt->execute([$resumeId]);
     $existingWorkIds = $existingWorkStmt->fetchAll(PDO::FETCH_COLUMN);
 
     $postedWorkIds = array_filter($workIds);
@@ -97,12 +97,47 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
             $update->execute([$jobTitles[$i], $companies[$i], $descs[$i], $wstarts[$i], $wends[$i], $id]);
         } else {
             $insert = $pdo->prepare("INSERT INTO Work (resumeId, jobTitle, companyName, jobDescription, startDate, endDate) VALUES (?, ?, ?, ?, ?, ?)");
-            $insert->execute([$selectedResumeId, $jobTitles[$i], $companies[$i], $descs[$i], $wstarts[$i], $wends[$i]]);
+            $insert->execute([$resumeId, $jobTitles[$i], $companies[$i], $descs[$i], $wstarts[$i], $wends[$i]]);
         }
     }
 
-    header("Location: resume.php?pageType=view&resumeId=$selectedResumeId");
+    header("Location: resume.php?pageType=view&resumeId=$resumeId");
     exit();
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete'])) {
+
+// Handle Education
+	$eduIds = $_POST['educationId'] ?? [];
+	$institutions = $_POST['institution'] ?? [];
+	$degrees = $_POST['degree'] ?? [];
+	$fields = $_POST['fieldOfStudy'] ?? [];
+	$starts = $_POST['startDate'] ?? [];
+	$ends = $_POST['endDate'] ?? [];
+	
+	$existingEduStmt = $pdo->prepare("SELECT educationId FROM Education WHERE resumeId = ?");
+	$existingEduStmt->execute([$resumeId]);
+	$existingEduIds = $existingEduStmt->fetchAll(PDO::FETCH_COLUMN);
+	
+	$postedEduIds = array_filter($eduIds);
+	$toDelete = array_diff($existingEduIds, $postedEduIds);
+	
+	foreach ($toDelete as $id) {
+		$del = $pdo->prepare("DELETE FROM Education WHERE educationId = ?");
+		$del->execute([$id]);
+	}
+	
+	for ($i = 0; $i < count($institutions); $i++) {
+		$id = $eduIds[$i];
+		if ($id) {
+			$update = $pdo->prepare("UPDATE Education SET institutionName=?, degree=?, fieldOfStudy=?, startDate=?, endDate=? WHERE educationId=?");
+			$update->execute([$institutions[$i], $degrees[$i], $fields[$i], $starts[$i], $ends[$i], $id]);
+		} else {
+			$insert = $pdo->prepare("INSERT INTO Education (resumeId, institutionName, degree, fieldOfStudy, startDate, endDate) VALUES (?, ?, ?, ?, ?, ?)");
+			$insert->execute([$resumeId, $institutions[$i], $degrees[$i], $fields[$i], $starts[$i], $ends[$i]]);
+		}
+	}
 }
 ?>
 
@@ -153,91 +188,94 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
     </div>
 
     <?php
-    $stmt = $pdo->prepare("SELECT userFirstName, userLastName FROM User WHERE userid = ?");
-    $stmt->execute([$userID]);
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    $query = "SELECT userFirstName, userLastName FROM User WHERE userid = '$userID'";
+    $userStmt = callQuery($pdo, $query, "Unable to retrieve user's personal information.");
+    $user = $userStmt->fetch();
     $fullName = $user ? $user['userFirstName'] . ' ' . $user['userLastName'] : "Unknown";
 
-    $eduStmt = $pdo->prepare("SELECT * FROM Education WHERE resumeId = ?");
-    $eduStmt->execute([$selectedResumeId]);
-    $educations = $eduStmt->fetchAll(PDO::FETCH_ASSOC);
+    $query = "SELECT * FROM Education WHERE resumeId =  '$resumeId'";
+    $eduStmt = callQuery($pdo, $query, "Error fetching user's education information");
 
-    $workStmt = $pdo->prepare("SELECT * FROM Work WHERE resumeId = ?");
-    $workStmt->execute([$selectedResumeId]);
-    $workHistory = $workStmt->fetchAll(PDO::FETCH_ASSOC);
+    $query = "SELECT * FROM Work WHERE resumeId = '$resumeId'";
+    $workStmt = callQuery($pdo, $query, "Error fetching user's work history");
     ?>
 
     <h1>Viewing <?= htmlspecialchars($fullName) ?>'s Resume</h1>
-
-    <form method="post">
-        <input type="hidden" name="resumeId" value="<?= htmlspecialchars($selectedResumeId) ?>">
+        <input type="hidden" name="resumeId" value="<?= htmlspecialchars($resumeId) ?>">
 
         <div class="infoTitle">
-            <button type="button" class="editBtn"><img src="garbage/pencil.png" alt="Edit"></button>
         </div>
+        <div id="resumeInfoMain" class="resumeInfo">
+            <form method="post">
+                <div id="educationSection" class="resumeInfo">
+                    <div class="fieldDiv">
+                        <h3>Education</h3>
+                        <button type="button" id="addEducationBtn"><img src="garbage/plus.png" alt="Add"></button>
+                    </div>
+									
+									<?php
+									while($row = $eduStmt->fetch()) {
+										?><div class="educationBlock">
+                      <input type="hidden" name="educationId[]" value="<?= htmlspecialchars($row['educationId']) ?>">
+                      <button type="button" class="editBtn"><img src="garbage/pencil.png" alt="Edit"></button>
+                      <button type="button" class="removeBtn"><img src="garbage/can.png" alt="Delete"></button><br>
+                      <label>Institution</label><br>
+                      <input type="text" name="institution[]" value="<?= htmlspecialchars($row['institutionName']) ?>" readonly><br>
+                      <label>Degree</label><br>
+                      <input type="text" name="degree[]" value="<?= htmlspecialchars($row['degree']) ?>" readonly><br>
+                      <label>Field of Study</label><br>
+                      <input type="text" name="fieldOfStudy[]" value="<?= htmlspecialchars($row['fieldOfStudy']) ?>" readonly><br>
+                      <label>Start Date</label><br>
+                      <input type="date" name="startDate[]" value="<?= $row['startDate'] ?>" readonly><br>
+                      <label>End Date</label><br>
+                      <input type="date" name="endDate[]" value="<?= $row['endDate'] ?>" readonly><br>
+                      <br>
+                      </div><br><br>
+									<?php } ?>
 
+                </div>
+                <input id="educationSubmit" type="submit" value="Submit" name="submit" style="display: none;">
+            </form>
+
+            <!-- === WORK SECTION === -->
+            <div id="workSection" class="resumeInfo">
+                <div class="fieldDiv">
+                    <h3>Work Experience</h3>
+                    <button type="button" id="addWorkBtn">Add More</button>
+                </div>
+							
+							<?php
+							while($row = $workStmt->fetch()) {
+								?>
+                  <div class="workBlock">
+                      <form method="post">
+                          <input type="hidden" name="workId[]" value="<?= htmlspecialchars($row['workId']) ?>">
+                          <label>Job Title</label>
+                          <input type="text" name="jobTitle[]" value="<?= htmlspecialchars($row['jobTitle']) ?>" readonly>
+                          <label>Company Name</label>
+                          <input type="text" name="companyName[]" value="<?= htmlspecialchars($row['companyName']) ?>" readonly>
+                          <label>Job Description</label>
+                          <textarea name="jobDescription[]" readonly><?= htmlspecialchars($row['jobDescription']) ?></textarea>
+                          <label>Start Date</label>
+                          <input type="date" name="workStartDate[]" value="<?= $row['startDate'] ?>" readonly>
+                          <label>End Date</label>
+                          <input type="date" name="workEndDate[]" value="<?= $row['endDate'] ?>" readonly>
+                          <button type="button" class="removeBtn">Remove</button>
+                          <br>
+                          <input type="submit" value="Submit" name="submit">
+                      </form>
+                  </div>
+							<?php } ?>
+            </div>
+
+            <!-- SUBMIT -->
+            <div class="btnDiv" style="display: none;">
+                <input type="submit" name="submit" value="Submit">
+                <button type="button" class="cancelBtn">Cancel</button>
+            </div>
+        </div>
         <!-- === EDUCATION SECTION === -->
-        <div id="educationSection" class="resumeInfo">
-            <div class="fieldDiv">
-                <h3>Education</h3>
-                <button type="button" id="addEducationBtn">Add More</button>
-            </div>
-
-            <?php
-            while($row = $eduStmt->fetch()) {
-            ?>
-                <div class="educationBlock">
-                    <input type="hidden" name="educationId[]" value="<?= htmlspecialchars($row['educationId']) ?>">
-                    <label>Institution</label>
-                    <input type="text" name="institution[]" value="<?= htmlspecialchars($row['institutionName']) ?>" readonly>
-                    <label>Degree</label>
-                    <input type="text" name="degree[]" value="<?= htmlspecialchars($row['degree']) ?>" readonly>
-                    <label>Field of Study</label>
-                    <input type="text" name="fieldOfStudy[]" value="<?= htmlspecialchars($row['fieldOfStudy']) ?>" readonly>
-                    <label>Start Date</label>
-                    <input type="date" name="startDate[]" value="<?= $row['startDate'] ?>" readonly>
-                    <label>End Date</label>
-                    <input type="date" name="endDate[]" value="<?= $row['endDate'] ?>" readonly>
-                    <button type="button" class="removeBtn">Remove</button>
-                    <hr>
-                </div>
-            <?php } ?>
-        </div>
-
-        <!-- === WORK SECTION === -->
-        <div id="workSection" class="resumeInfo">
-            <div class="fieldDiv">
-                <h3>Work Experience</h3>
-                <button type="button" id="addWorkBtn">Add More</button>
-            </div>
-
-            <?php
-            while($row = $workStmt->fetch()) {
-            ?>
-                <div class="workBlock">
-                    <input type="hidden" name="workId[]" value="<?= htmlspecialchars($row['workId']) ?>">
-                    <label>Job Title</label>
-                    <input type="text" name="jobTitle[]" value="<?= htmlspecialchars($row['jobTitle']) ?>" readonly>
-                    <label>Company Name</label>
-                    <input type="text" name="companyName[]" value="<?= htmlspecialchars($row['companyName']) ?>" readonly>
-                    <label>Job Description</label>
-                    <textarea name="jobDescription[]" readonly><?= htmlspecialchars($row['jobDescription']) ?></textarea>
-                    <label>Start Date</label>
-                    <input type="date" name="workStartDate[]" value="<?= $row['startDate'] ?>" readonly>
-                    <label>End Date</label>
-                    <input type="date" name="workEndDate[]" value="<?= $row['endDate'] ?>" readonly>
-                    <button type="button" class="removeBtn">Remove</button>
-                    <hr>
-                </div>
-            <?php } ?>
-        </div>
-
-        <!-- SUBMIT -->
-        <div class="btnDiv" style="display: none;">
-            <input type="submit" name="submit" value="Submit">
-            <button type="button" class="cancelBtn">Cancel</button>
-        </div>
-    </form>
+        
 
     <div class="footerDiv">
         <p>Copyright 2025<span>&copy;</span>Resumate</p>
@@ -273,6 +311,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
         });
         document.querySelector('.btnDiv').style.display = 'block';
         document.querySelector('.editBtn').style.display = 'none';
+        document.querySelector('#educationSubmit').style.display = "block";
     });
 
     document.querySelector('.cancelBtn').addEventListener('click', () => {
@@ -282,6 +321,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
         });
         document.querySelector('.btnDiv').style.display = 'none';
         document.querySelector('.editBtn').style.display = 'inline-block';
+        document.querySelector('#educationSubmit').style.display = "none";
     });
 
     function createRemoveButton(containerSelector, blockClass) {
@@ -300,6 +340,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
                 let blocks = container.querySelectorAll(`.${blockClass}`);
                 if (blocks.length > 1) {
                     block.remove();
+                    
                 } else {
                     block.querySelectorAll('input, textarea').forEach(input => {
                         if (input.type === 'hidden') {
@@ -308,21 +349,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
                             input.value = '';
                         }
                     });
+                    
                 }
+                document.querySelector('#educationSubmit').style.display = "block";
             };
         });
     }
-
+    // 'ADD MORE' BUTTONS
     document.querySelector('#addEducationBtn').addEventListener('click', () => {
         let container = document.querySelector('#educationSection');
         let first = container.querySelector('.educationBlock');
         let clone = first.cloneNode(true);
 
-        clone.querySelectorAll('input, textarea').forEach(input => {
+        clone.querySelectorAll('input').forEach(input => {
             input.value = '';
             input.removeAttribute('readonly');
         });
-
+        document.querySelector('#educationSubmit').style.display = "block";
         clone.querySelector('input[name="educationId[]"]').value = ''; // clear ID
         container.appendChild(clone);
         createRemoveButton('#educationSection', 'educationBlock');
