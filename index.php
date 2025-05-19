1a3c6febbb86449b739c761d87ac3f247d75b5df
trent-witkowski <165222713+trent-witkowski@users.noreply.github.com>
2025-05-18 21:52:53 -0500

    Update index.php
    Removed code that was not needed

--- index.php
+++ index.php
@@ -57,63 +57,7 @@ if (isset($_GET['logout'])) {
         </table>
     </div>
 
-    <div class="main"><?php  // TODO - Remove the following two lines of code from
-                        // our production code.
-                        ini_set('display_errors', '1');
-                        error_reporting(-1);  // level value of -1 says to display all PHP errors
-
-                        // Include the sanitizeString function (sanitize.php)
-                        //
-                        // include vs require
-                        //
-                        // include imports code from a file giving a warning message
-                        // if the file cannot be opened for any reason.
-                        //
-                        // require also imports code from a file, but gives a fatal
-                        // program-ending error if the file cannot be opened.
-                        //
-                        require 'sanitize.php';
-
-                        // Define callQuery() helper function to run a passed-in
-                        // query string.
-                        require 'callQuery.php';
-
-                        try {
-
-                            // Create an instance of the PDO class
-                            // $pdo = new PDO('connectionString', 'userName', 'password');
-                            $pdo = new PDO('mysql:host=sql111.infinityfree.com:3306;dbname=if0_38758969_resumatedb', 'if0_38758969', 'CVTCit2025');
-                        } catch (PDOException $ex) {
-
-                            // Note: remove $ex->getMessage() from production code so we don't
-                            // reveal too much detailed information.  TODO for production mode
-                            $error = 'Unable to connect to the database server<br><br>' . $ex->getMessage();
-
-                            include 'error.html.php';
-                            throw $ex;
-                            //exit();
-
-                        }
-                        //
-                        // Include the code to connect to our DB and login to it
-                        //
-                        // require 'dbConnect.php';
-
-                        $query = "SELECT *
-            FROM User;";
-                        // had to quote $newBookTitle as it contains a string value
-
-                        $errorMsg = 'Error fetching User information';
-
-                        $thisPage = sanitizeString(INPUT_SERVER, 'PHP_SELF');
-                        $user = callQuery($pdo, $query, $errorMsg);
-
-                        //  while ($row = $user->fetch()) {
-                        //    echo $row['userLastName'];
-                        //  }
-
-
-                        ?>
+    <div class="main">
         <!--            HTML START-->
         <div class="missionDiv">
             <p id="missionStatement">At Resumate, our mission is to empower individuals to take control of their professional journey by
@@ -132,17 +76,6 @@ if (isset($_GET['logout'])) {
             </form>
         </div>
         <!--            HTML END  -->
-        <?php
-        // TODO
-        //      check to see if user is signed in when clicking the "Get STarted!" btn.
-        //      If yes they either go to user account or resume browsing depending on type of account.
-        //      If the user isn't signed in, bring them to login page'.
-
-        //      Maybe can put the check on the user account/browse pages and if you are not signed in then you have to sign in.
-        //      This would be a security check and prevent people from accessing random pages using url.
-
-
-        ?>
     </div>
     <div class="footerDiv">
         <p>Copyright 2025<span>&copy;</span>Resumate</p>
